163276382ab4a12a046f4532a55e01e5f0c59489
Fantik365 <timfad25@gmail.com>
2025-11-02 20:05:45 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hallo world");
+        System.out.println("hallo world from task 2");
     }
 }
